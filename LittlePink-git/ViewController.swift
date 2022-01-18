25ea7733890462e3fa91896a94e9cea1c323d62a
ruanmin <ruanmin@agora.io>
2022-01-19 00:38:21 +0800

    颜色配置

--- LittlePink-git/ViewController.swift
+++ LittlePink-git/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("做了UI")
-        print("做了UI2")
+        tabBarController?.tabBar.barTintColor = .black
     }
 
 
